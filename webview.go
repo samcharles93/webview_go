f25713a96c9ec1dab820a630c98106b977c0f1f3
Sam Catlow <sam@catlow.tech>
2026-03-13 21:38:23 +1100

    feat: modernize the library:   - Refactored to runtime/cgo.Handle: Replaced manual handle management with safer pattern.   - Updated to Go 1.25+ standards: Replaced interface{} with any.   - Fixed memory leaks: Added proper handle deletion in Unbind and Destroy, and removed the leaking binding_context from glue.c.   - Added new APIs: Exposed Version() and NativeHandle() from the webview library.

--- webview.go
+++ webview.go
@@ -25,12 +25,12 @@ void CgoWebViewUnbind(webview_t w, const char *name);
 */
 import "C"
 import (
+	"encoding/json"
+	"errors"
 	_ "github.com/samcharles93/webview_go/libs/mswebview2"
 	_ "github.com/samcharles93/webview_go/libs/mswebview2/include"
 	_ "github.com/samcharles93/webview_go/libs/webview"
 	_ "github.com/samcharles93/webview_go/libs/webview/include"
-	"encoding/json"
-	"errors"
 	"reflect"
 	"runtime"
 	"runtime/cgo"
